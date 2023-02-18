a06bd292d82dea09ddcdb7e6e427fa9672168321
Faidide <quentin.faidide@gmail.com>
2023-02-18 14:23:56 +0100

    doc: add example

--- gochunks/gochunks_test.go
+++ gochunks/gochunks_test.go
@@ -1,6 +1,8 @@
 package gochunks
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -104,4 +106,27 @@ func TestUnitChunks3(t *testing.T) {
 		idsMap[chunks[0][id]] = struct{}{}
 	}
 
+}
+
+func ExampleMakeConcurrencyChunks() {
+	numbersToHalves := []int{4, 10, 30, 200, 3090}
+	halvedNumbers := make([]int, 5)
+
+	concurrentIdsChunks := MakeConcurrencyChunks(5, 2)
+
+	// iterate over chunks with their own waitgroup synchronously
+	for _, cChunk := range concurrentIdsChunks {
+		var wg sync.WaitGroup
+		wg.Add(len(cChunk))
+		// for each chunk conrrent task pop a worker and perform task
+		for _, taskId := range cChunk {
+			go func(i int) {
+				halvedNumbers[i] = numbersToHalves[i] >> 1
+				wg.Done()
+			}(taskId)
+		}
+		wg.Wait()
+	}
+	fmt.Println(halvedNumbers)
+	// Output: {2, 5, 15, 100, 1545}
 }
